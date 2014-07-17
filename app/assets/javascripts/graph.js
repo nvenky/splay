ac91c09f2bdef135e059f394afebe263567edc15
Venkatesh Nannan <nvenky@gmail.com>
2014-07-18 07:39:19 +1000

    Added Sl No to the summary table for easy reference

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -5,9 +5,18 @@ var drawGraph = function(data){
       columns: [ data[0], data[1] ]
     },
     grid: {
-        y: {
-            lines: [{value: 0, text: '0 AXIS'}]
+      y: {
+        lines: [{value: 0, text: '0 AXIS'}]
+      }
+    },
+    axis : {
+      x : {
+        tick: {
+          //format: d3.format("$,")
+          format: function (d) { return d + 1; }
         }
+      }
     }
-});
+
+  });
 };
---
+++ app/assets/javascripts/handlebar_helpers.js
@@ -0,0 +1,3 @@
+Handlebars.registerHelper("increment", function(a) {
+  return a + 1;
+});
