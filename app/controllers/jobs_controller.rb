0df51568c67fc77cae33ee856e767af5ff6dbb06
Venkatesh Nannan <nvenky@gmail.com>
2014-07-21 22:24:53 +1000

    Minor refactoring

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,18 +1,24 @@
 class JobsController < ApplicationController
+
+  def run
+    load_event_types
+    load_markets
+    update_markets
+    render text: 'Jobs completed successfully'
+  end
+
+  private
   def load_event_types
     EventType.load
-    render text: 'Load event types Success'
   end
 
   def load_markets
     Market.load EventType.where("name like 'Horse%'").first.api_id
     Market.load EventType.where("name like 'Greyhound%'").first.api_id
-    render text: 'Load market Success'
   end
 
   def update_markets
     Market.update_closed_markets
-    render text: 'Update market Success'
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Betfair::Application.routes.draw do
       post :run
     end
   end
-  match 'jobs/load_event_types', to: 'jobs#load_event_types', via: :get
-  match 'jobs/load_markets', to: 'jobs#load_markets', via: :get
-  match 'jobs/update_markets', to: 'jobs#update_markets', via: :get
+
+  match 'jobs/run', to: 'jobs#run', via: :get
+  match 'home', to: 'home#new', via: :get
+
   root 'simulations#new'
 
   #Authentication
